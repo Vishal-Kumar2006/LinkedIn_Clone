42a22912978f3f1dd2fb876b4dacdde4a46fdce3
Vishal Kumar <vishalkumar438455@gmail.com>
2025-10-06 18:09:29 +0530

    Code done till upload profile photo backend

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,16 +1,34 @@
 import { Router } from "express";
+import multer from "multer";
 import {
   userActiveCheck,
   register,
   login,
+  uploadProfilePicture,
+  updateUserProfile,
 } from "../controllers/users.controllers.js";
 
 const router = Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const uploads = multer({ storage: storage });
+
+router
+  .route("/update_profile_picture")
+  .post(uploads.single("profile_picture"), uploadProfilePicture);
+
 router.route("/").get(userActiveCheck);
-
 router.route("/register").post(register);
-
 router.route("/login").post(login);
+router.route("/user_update").post(updateUserProfile);
 
 export default router;
