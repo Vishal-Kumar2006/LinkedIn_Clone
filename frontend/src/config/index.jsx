2e9d939cb37939797fab73e49042bcac18356b1c
Vishal Kumar <vishalkumar438455@gmail.com>
2026-05-13 21:41:50 +0530

    Server Deployed: Updating Backend URL

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://localhost:9090";
+export const BASE_URL = "https://linkedin-clone-4j3q.onrender.com";
 export const clientServer = axios.create({
   baseURL: BASE_URL,
 });
