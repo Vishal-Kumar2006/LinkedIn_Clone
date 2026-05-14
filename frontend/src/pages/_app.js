8dbfa68a12578012e903aa67b25c6b002539a9e0
Vishal Kumar <vishalkumar438455@gmail.com>
2026-05-14 06:44:48 +0530

    Upadted Font Family

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,13 +1,19 @@
 import { store } from "@/config/redux/store.js";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Provider store={store}>
+    <Provider store={store}>
+      <main className={poppins.className}>
         <Component {...pageProps} />
-      </Provider>
-    </>
+      </main>
+    </Provider>
   );
 }
